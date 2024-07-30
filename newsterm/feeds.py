5136d76b2804302bdf1bc768647d9ee779ce0fac
Ben Collier <bencollier@fastmail.com>
2024-07-30 13:40:39 +0100

    Remove markup from entries.

--- newsterm/feeds.py
+++ newsterm/feeds.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime
 from dateutil import parser
 from pytz import timezone
 
@@ -17,14 +19,24 @@ def get_latest_updates(sources: dict, locality: timezone) -> list:
     for source_name, source_url in sources["sources"].items():
         latest_update = feedparser.parse(source_url)
         for entry in latest_update.entries:
-
-            published_dt = parser.parse(entry.published)
-            localised_datetime = published_dt.astimezone(locality)
-
-            new_story = Story(source=source_name,
-                              title=entry.title,
-                              summary=entry.summary,
-                              datetime=localised_datetime)
+            try:
+                published_dt = parser.parse(entry.published)
+                localised_datetime = published_dt.astimezone(locality)
+                new_story = Story(source=source_name,
+                                  title=entry.title,
+                                  summary=remove_html_markup(entry.summary),
+                                  datetime=localised_datetime)
+            except AttributeError:
+                published_dt = datetime.now(locality)
+                new_story = Story(source=source_name,
+                                  title="ERROR. This source is missing a required attribute. Check the RSS.",
+                                  summary="This can happen if a publication omits a 'published' datetime.",
+                                  datetime=published_dt)
 
             latest_updates.append(new_story)
     return latest_updates
+
+
+def remove_html_markup(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
